db41095c6f6b25d5e4041b3bf0e4690cc8735cc8
DinarasN <119352434+DinarasN@users.noreply.github.com>
2023-01-08 15:04:09 +0500

    init: загрузила папку

---
+++ src/index.js
@@ -0,0 +1,9 @@
+module.exports = function reverse (num) {
+    let result = 0;
+    while (num) {
+      result = result * 10 + num % 10;
+      num = Math.floor(num / 10);
+    }
+  
+    return result;
+}
